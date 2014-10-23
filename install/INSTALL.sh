70019bd5b09c44e9fd630b8c573e1825dbfb1a9c
Shashank Khasare <shashank.khasare@gmail.com>
2014-10-24 00:31:00 +0530

    Added folders command_gen, install, items, magic_spells, players

---
+++ install/INSTALL.sh
@@ -0,0 +1,4 @@
+sudo apt-get install freeglut3-dev
+sudo apt-get install python-numpy
+sudo apt-get install libglew-dev
+sudo apt-get install libglfw-dev
---
+++ items/item.h
@@ -0,0 +1,17 @@
+#ifdef ITEM_H
+typedef struct item{
+	int item_id;
+	int movement_speed;
+	int attack_speed;
+	int health;
+	int damage;
+	int temp_heal;
+	int required_space;
+	int quantity;
+}item;
+
+/**
+  * Creates item by reading filename from items folder. If filename does not exist, game exits.
+**/  
+item * init_item(char *filename);
+#endif
