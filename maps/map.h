965c9e3d2fb124040a6374df36338667765662d6
Shashank Khasare <shashank.khasare@gmail.com>
2014-10-25 23:52:47 +0530

    Modified the map files and mapgen.py

---
+++ maps/map.h
@@ -0,0 +1,66 @@
+#ifdef MAP_H
+
+#define TEMPLE_BORDER		'T'
+#define JUNGLE_TREE		'J'
+#define TEAM_A_SPAWN_SYM	'A'
+#define TEAM_B_SPAWN_SYM	'B'
+#define FREE_SPACE		'.'
+
+#define TEAM_A			0
+#define TEAM_B			1
+
+typedef struct map_t{
+	int rows;
+	int cols;
+	char **terrain;
+	char **adj_mat;
+	temple temple_a, temple_b;
+	char spawn_locations[2][2];
+}map;
+
+typedef struct path_t{
+	char path[2500][2];
+	int last_path_index;
+}path_t;
+
+typedef struct point_t{
+	int x, y;
+}point;
+
+typedef struct points_list{
+	point * points;
+	int num_of_points;
+}points_list;
+
+
+/**
+  * Creates map by reading filename from map folder. If filename does not exist, game exits.
+**/  
+map * init_map(char *filename);
+
+/**
+  * Returns m->temple_location[index].
+**/  
+char * map_temple_location(int index, map *m);
+
+/**
+  * If [dest_x, dest_y] is 'J', 'A', 'B' then simply sets last_path_index to -1 and return.
+  * If [dest_x, dest_y] is unreachable point from [curr_x, curr_y], then simply sets last_path_index to -1 and return.
+  * If [dest_x, dest_y] is 'a' or 'b', then 1st feasible point, of all points sorted by euclidean distance from (curr_x, curr_y),
+  * defining boundary of corresponding temple, is considered to find path.
+  * Writes the path(i.e sequence of [x,y] points) between [curr_x, curr_y] &
+  * [dest_x, dest_y] into path_t.path and updates path_t.last_path_index. 
+**/  
+path_t construct_path(map *m, int curr_x, int curr_y, int dest_x, int dest_y);
+
+/**
+  * Returns euclidean distance between (x1, y1) and (x2, y2).
+**/  
+float euclidean_dist(int x1, int y1, int x2, int y2);
+
+/**
+  * Returns 1 if straight line (constucted using naive_line_drawaing_algo() Source: Wikipedia) 
+  * between (x1, y1) and (x2, y2) has only '.'s. Else return 0;
+**/  
+int is_line_of_sight_clear(int x1, int y1, int x2, int y2);
+#endif
--- maps/mapgen.py
+++ maps/mapgen.py
@@ -19,6 +19,9 @@ def Fill(Map, row, col, char):
 		return
 
 def DrawCircle(x0, y0, radius, code, Map, char):
+	Coord_list = set()
+	tmp = []
+	#Mid-point circle Algorithm
 	y0 = -y0;
 	x = radius
 	y = 0
@@ -28,9 +31,11 @@ def DrawCircle(x0, y0, radius, code, Map, char):
 		if code == LOWERQUAD:
 			Map[x + x0][-y + y0] = char
 			Map[y + x0][-x + y0] = char
+			tmp.append([ x + x0, -y + y0, y + x0, -x + y0])
 		if code == UPPERQUAD:	
 			Map[-x + x0][y + y0] = char
 			Map[-y + x0][x + y0] = char
+			tmp.append([-x + x0, y + y0, -y + x0, x + y0])
 		if code == FULL:
 			Map[x + x0][y + y0] = char
 			Map[y + x0][x + y0] = char
@@ -40,12 +45,20 @@ def DrawCircle(x0, y0, radius, code, Map, char):
 			Map[-y + x0][-x + y0] = char
 			Map[x + x0][-y + y0] = char
 			Map[y + x0][-x + y0] = char
+			tmp.append([x + x0, y + y0, y + x0, x + y0, -x + x0, y + y0, -y + x0, x + y0, -x + x0, -y + y0, -y + x0, -x + y0, -y + x0, -x + y0, y + x0, -x + y0])
 		y += 1
 		if radiusError < 0:
 			radiusError += 2 * y + 1
 		else:
 			x -= 1
 			radiusError += 2 * (y - x + 1)
+	
+	for i in tmp:
+		j = 0
+		while j < len(i):
+			Coord_list.add((i[j] % len(Map[0]), i[j+1] % len(Map)))	
+			j += 2
+	return Coord_list		
 
 def GenCoordList(capacity, Map):
 	s1 = 0
@@ -65,11 +78,18 @@ def GenCoordList(capacity, Map):
 	return L, U
 
 def Print(Map):
+	print "MAP:"
 	for i in Map:
 		s = ""
 		for j in i:
 			s += j+' '
 		print s
+
+def print_Coord_list(Coord_list):
+	s = ""
+	for i in Coord_list:
+		s += str(i) + " "
+	return s	
 
 rows = 50
 cols = 50
@@ -77,16 +97,24 @@ cols = 50
 Map = [['.' for i in range(cols)] for j in range(rows)]
 
 # Draw Territories
-DrawCircle(-1, 0, rows/8, UPPERQUAD, Map, 'A')
+Coord_list = DrawCircle(-1, 0, rows/8, UPPERQUAD, Map, 'A')
+print "TEAM_A_SPAWN_BORDER: ", print_Coord_list(Coord_list)
 Fill(Map, rows-1, 0, 'A')
-DrawCircle(0, 1, rows/8, LOWERQUAD, Map, 'B')
+
+Coord_list = DrawCircle(0, 1, rows/8, LOWERQUAD, Map, 'B')
+print "TEAM_B_SPAWN_BORDER: ", print_Coord_list(Coord_list)
 Fill(Map, 0, cols-1, 'B')
-DrawCircle(cols/2-cols/6, rows/2+rows/6, rows/12, FULL, Map, 'a')
+
+Coord_list = DrawCircle(cols/2-cols/6, rows/2+rows/6, rows/12, FULL, Map, 'a')
+print "TEAM_A_TEMPLE_BORDER: ", print_Coord_list(Coord_list)
+
+Coord_list = DrawCircle(cols/2+cols/6, rows/2-rows/6, rows/12, FULL, Map, 'b')
+print "TEAM_B_TEMPLE_BORDER: ", print_Coord_list(Coord_list)
+
 Fill(Map, rows/2-rows/6, cols/2-cols/6, 'a')
-print "A_TEAM: ", rows/2-rows/6," ", cols/2-cols/6
-DrawCircle(cols/2+cols/6, rows/2-rows/6, rows/12, FULL, Map, 'b')
+print "A_TEAM_TEMPLE_CENTER: ", str((rows/2-rows/6, cols/2-cols/6))
 Fill(Map, rows/2+rows/6, cols/2+cols/6, 'b')
-print "B_TEAM: ", rows/2+rows/6," ", cols/2+cols/6
+print "B_TEAM_TEMPLE_CENTER: ", str((rows/2+rows/6, cols/2+cols/6))
 
 # Draw Jungle
 L, U = GenCoordList(rows*cols/25, Map)
