8290b8d5c1bcb22a88bc67e313dd9f38af79c20a
Shashank Khasare <shashank.khasare@gmail.com>
2014-10-30 14:22:15 +0530

    Added Battle margin.

--- maps/mapgen.py
+++ maps/mapgen.py
@@ -8,7 +8,7 @@ FULL = 3
 
 def Fill(Map, row, col, char):
 	if row<len(Map) and col<len(Map[0]) and row >= 0 and col >= 0:
-		if not Map[row][col] == '.':
+		if not (Map[row][col] == '.' or Map[row][col] == 'w' ):
 			return
 		Map[row][col] = char
 		Fill(Map, row-1, col, char)
@@ -69,10 +69,10 @@ def GenCoordList(capacity, Map):
 	while s1 < capacity or s2 < capacity :
 		x = randrange(len(Map[0])-1)
 		y = randrange(len(Map)-1)
-		if x > y and s1 < capacity and Map[y][x] == '.' and not U.__contains__([x,y]):
+		if x > y and s1 < capacity and (Map[y][x] == '.' or Map[y][x] == 'w' ) and not U.__contains__([x,y]):
 			U.append([x,y])
 			s1 += 1
-		elif x < y and s2 < capacity  and Map[y][x] == '.' and not L.__contains__([x,y]):
+		elif x < y and s2 < capacity  and (Map[y][x] == '.' or Map[y][x] == 'w' ) and not L.__contains__([x,y]):
 			L.append([x,y])
 			s2 += 1	
 	return L, U
@@ -91,10 +91,42 @@ def print_Coord_list(Coord_list):
 		s += str(i) + " "
 	return s	
 
-rows = 50
-cols = 50
+def DrawMargin(Map, offset):
+	i_max = len(Map)
+	j_max = len(Map[0])
+	
+	i = 0
+	for j in range(offset, j_max):
+		if j % 10 != 0:
+			Map[i][j] = 'W'
+		i += 1
+	
+	for k in range(1, offset+1):
+		i = 0
+		for j in range(offset-k, j_max):
+			Map[i][j] = 'w'
+			i += 1	
+	
+	for k in range(1, offset+1):
+		j = 0
+		for i in range(offset-k, i_max):
+			Map[i][j] = 'w'
+			j += 1
+		
+	j = 0
+	for i in range(offset, i_max):
+		if i % 10 != 0:
+			Map[i][j] = 'W'
+		j += 1	
+	
+
+rows = 75
+cols = 75
 
 Map = [['.' for i in range(cols)] for j in range(rows)]
+
+#Draw Fight Margin
+DrawMargin(Map, rows/6)
 
 # Draw Territories
 Coord_list = DrawCircle(-1, 0, rows/8, UPPERQUAD, Map, 'A')
@@ -105,10 +137,10 @@ Coord_list = DrawCircle(0, 1, rows/8, LOWERQUAD, Map, 'B')
 print "TEAM_B_SPAWN_BORDER: ", print_Coord_list(Coord_list)
 Fill(Map, 0, cols-1, 'B')
 
-Coord_list = DrawCircle(cols/2-cols/6, rows/2+rows/6, rows/12, FULL, Map, 'a')
+Coord_list = DrawCircle(cols/2-cols/6, rows/2+rows/6, int(rows*0.065), FULL, Map, 'a')
 print "TEAM_A_TEMPLE_BORDER: ", print_Coord_list(Coord_list)
 
-Coord_list = DrawCircle(cols/2+cols/6, rows/2-rows/6, rows/12, FULL, Map, 'b')
+Coord_list = DrawCircle(cols/2+cols/6, rows/2-rows/6, int(rows*0.065), FULL, Map, 'b')
 print "TEAM_B_TEMPLE_BORDER: ", print_Coord_list(Coord_list)
 
 Fill(Map, rows/2-rows/6, cols/2-cols/6, 'a')
@@ -117,7 +149,7 @@ Fill(Map, rows/2+rows/6, cols/2+cols/6, 'b')
 print "B_TEAM_TEMPLE_CENTER: ", str((rows/2+rows/6, cols/2+cols/6))
 
 # Draw Jungle
-L, U = GenCoordList(rows*cols/25, Map)
+L, U = GenCoordList(rows*cols*0.025, Map)
 for j in [L,U]:
 	for i in j:
 		Map[i[1]][i[0]] = 'J'
