441442ca656dde597efd6ca4babe6df87931af12
Shashank Khasare <shashank.khasare@gmail.com>
2014-10-31 15:48:36 +0530

    Mapgen.py and README updated.

--- maps/mapgen.py
+++ maps/mapgen.py
@@ -88,17 +88,21 @@ def Print(Map):
 def print_Coord_list(Coord_list):
 	s = ""
 	for i in Coord_list:
-		s += str(i) + " "
+		s += str(i[0])+ " "+ str(i[1])+ " "
 	return s	
 
 def DrawMargin(Map, offset):
 	i_max = len(Map)
 	j_max = len(Map[0])
 	
+	interval = 5
+	
 	i = 0
 	for j in range(offset, j_max):
-		if j % 10 != 0:
+		if j % interval != 0:
 			Map[i][j] = 'W'
+		else:
+			Map[i][j] = 'E'	
 		i += 1
 	
 	for k in range(1, offset+1):
@@ -115,8 +119,10 @@ def DrawMargin(Map, offset):
 		
 	j = 0
 	for i in range(offset, i_max):
-		if i % 10 != 0:
+		if i % interval != 0:
 			Map[i][j] = 'W'
+		else:
+			Map[i][j] = 'E'
 		j += 1	
 	
 
@@ -125,28 +131,33 @@ cols = 75
 
 Map = [['.' for i in range(cols)] for j in range(rows)]
 
+
+#Print Rows and cols:
+print "ROWS: ", rows
+print "COLS: ", cols
 #Draw Fight Margin
+print "WAR_BORBER_OFFSET: ", rows/6
 DrawMargin(Map, rows/6)
 
 # Draw Territories
 Coord_list = DrawCircle(-1, 0, rows/8, UPPERQUAD, Map, 'A')
-print "TEAM_A_SPAWN_BORDER: ", print_Coord_list(Coord_list)
+print "TEAM_A_SPAWN_BORDER: ", len(Coord_list), print_Coord_list(Coord_list)
 Fill(Map, rows-1, 0, 'A')
 
 Coord_list = DrawCircle(0, 1, rows/8, LOWERQUAD, Map, 'B')
-print "TEAM_B_SPAWN_BORDER: ", print_Coord_list(Coord_list)
+print "TEAM_B_SPAWN_BORDER: ", len(Coord_list), print_Coord_list(Coord_list)
 Fill(Map, 0, cols-1, 'B')
 
 Coord_list = DrawCircle(cols/2-cols/6, rows/2+rows/6, int(rows*0.065), FULL, Map, 'a')
-print "TEAM_A_TEMPLE_BORDER: ", print_Coord_list(Coord_list)
+print "TEAM_A_TEMPLE_BORDER: ", len(Coord_list), print_Coord_list(Coord_list)
 
 Coord_list = DrawCircle(cols/2+cols/6, rows/2-rows/6, int(rows*0.065), FULL, Map, 'b')
-print "TEAM_B_TEMPLE_BORDER: ", print_Coord_list(Coord_list)
+print "TEAM_B_TEMPLE_BORDER: ", len(Coord_list), print_Coord_list(Coord_list)
 
 Fill(Map, rows/2-rows/6, cols/2-cols/6, 'a')
-print "A_TEAM_TEMPLE_CENTER: ", str((rows/2-rows/6, cols/2-cols/6))
+print "A_TEAM_TEMPLE_CENTER: ", str(rows/2-rows/6), str(cols/2-cols/6)
 Fill(Map, rows/2+rows/6, cols/2+cols/6, 'b')
-print "B_TEAM_TEMPLE_CENTER: ", str((rows/2+rows/6, cols/2+cols/6))
+print "B_TEAM_TEMPLE_CENTER: ", str(rows/2+rows/6), str(cols/2+cols/6)
 
 # Draw Jungle
 L, U = GenCoordList(rows*cols*0.025, Map)
