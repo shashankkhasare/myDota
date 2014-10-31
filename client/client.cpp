0f4167971f3f2479cf8b5c235ed3428af190dc68
Akshay Jain <uniquejainakshay@gmail.com>
2014-10-31 19:33:16 +0530

    Client added

---
+++ client/client.cpp
@@ -0,0 +1,21 @@
+class Client {
+
+
+	// Client is a simple class which will look after the following tasks:
+	// 	1. Listening the video channel and render it on the screen of the client. The information will be just the location 
+	// 		of differnt objects , players items and so on. 
+	// 	2. Define an interface to the command generation logic which could a human player or a simple bot generating the input commands.
+	// 		Sending these commands to the input buffer of the server ( through another channel differnt from the above video channel). 
+	// 		So there are 2 socket connections to the server for each client class. 
+	
+
+
+
+	/*
+	 * Data structures 
+	 * */
+
+
+
+
+}
