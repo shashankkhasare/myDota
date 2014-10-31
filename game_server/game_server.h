6438e334909ae77e796076e6b1bf174622e05821
Akshay Jain <uniquejainakshay@gmail.com>
2014-10-31 14:22:41 +0530

    Game server added

--- game_server/game_server.h
+++ game_server/game_server.h
@@ -7,3 +7,22 @@ point_t get_curr_pos(int h_id);
   * Listens for commands;
 **/  
 void recv_cmd();
+
+
+
+
+class Server {
+
+
+	// map 
+	// player array
+	// 2 threads, 1 constantly broadcasting the rendering information
+	// 	      2 constantly reading the input buffer, which would be filled by the client only. Redirecting each command received 
+	// 	        to the appropriate player connected to the server
+	// Each player is an independent thread running on the server machine, which reads its input buffer which is being filled by the server
+	// and accordingly updating the game data structures, for eg the map, the item status and so on. 
+	
+
+
+
+}
